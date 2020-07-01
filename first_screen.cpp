1e5d04e61d5869002551e8c0a9f06993633eb819
leejaeheon446 <65205688+leejaeheon446@users.noreply.github.com>
2020-07-01 13:06:52 +0900

    Update and rename first_screen to first_screen.cpp

--- first_screen.cpp
+++ first_screen.cpp
@@ -1,4 +1,7 @@
-#include"f.h"
+```cpp
+// first_screen.cpp
+// 실행 시 첫 화면
+#include "f.h"
 
 void first_screen()
 {	
@@ -13,8 +16,13 @@ void first_screen()
 	cout << "                     ■■                                              \n";
 	cout << endl << endl << endl;
 	cout << "					<계속하려면 Enter를 눌러주세요.>" << endl << endl;
+	
+	
 	char ch;
 	cin.get(ch);
 	if (ch == '\n')
 		system("cls");
+	else
+		exit(1);
 }
+
