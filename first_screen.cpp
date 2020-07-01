615b3139ca023d98bd767b1fc76bbce157136165
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2020-07-01 16:49:14 +0900

    Update first_screen.cpp

--- first_screen.cpp
+++ first_screen.cpp
@@ -5,6 +5,17 @@
 
 void first_screen()
 {	
+	cout << endl << endl;
+	cout << "┏━━━━━━ Help Cinema━━━━━━┓" << endl;
+	cout << "┃                                    ┃" << endl;
+	cout << "┃ 1. 팝콘     :  8000원              ┃" << endl;
+	cout << "┃ 2. 오징어   :  5000원              ┃" << endl;
+	cout << "┃ 3. 나쵸     :  3000원              ┃" << endl;
+	cout << "┃ 4. 종료                            ┃" << endl;
+	cout << "┃                                    ┃" << endl;
+	cout << "┗━━━━━━━━━━━━━━━━━━┛" << endl;
+	cout << "					<원하는 항목을 선택하시오.>" << endl << endl;
+	
 	cout << endl << endl << endl;
 	cout << "                                 ■■■■    ■■■■     ■        ■ \n";
 	cout << "                                ■      ■  ■      ■    ■        ■ \n";
