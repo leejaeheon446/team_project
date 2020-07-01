5273cce3e4ef95339ba21a35508260f263c5b9ec
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2020-07-01 16:52:01 +0900

    Create show_menu.cpp
    Create show_menu.cpp

---
+++ show_menu.cpp
@@ -0,0 +1,19 @@
+```cpp
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "┏━━━━━━ Help Cinema━━━━━━┓" << endl;
+	cout << "┃                                    ┃" << endl;
+	cout << "┃ 1. 팝콘     :  8000원              ┃" << endl;
+	cout << "┃ 2. 오징어   :  5000원              ┃" << endl;
+	cout << "┃ 3. 나쵸     :  3000원              ┃" << endl;
+	cout << "┃ 4. 종료                            ┃" << endl;
+	cout << "┃                                    ┃" << endl;
+	cout << "┗━━━━━━━━━━━━━━━━━━┛" << endl;
+	cout << " ▶ 원하는 항목을 선택하시오 : ";
+}
